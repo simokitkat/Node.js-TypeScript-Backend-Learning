21ba0a19547e34ae4db310b6ede36c0f4157fb8e
simokitkat <simokitkat@yahoo.com>
2026-05-27 22:03:08 +0300

    feat(nodejs): add local modules example with add and subtract functions

---
+++ Node.js/localModules/add.js
@@ -0,0 +1,5 @@
+const add = (a, b) => a + b;
+
+const sum = add(1, 2);
+
+console.log("hello from add.js. the sum is", sum); // 3
---
+++ Node.js/localModules/index.js
@@ -0,0 +1,7 @@
+require("./add");
+
+const subtract = require("./subtract");
+
+console.log("hello from index.js");
+
+console.log(subtract(25, 20));
